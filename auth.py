2b417e07ebc685ba711fb4a35892a0ee13e34a2b
Ore <aboloresanni001@gmail.com>
2026-04-06 00:36:43 +0100

    updated

--- auth.py
+++ auth.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "120"))
 
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL", "admin@example.com")
