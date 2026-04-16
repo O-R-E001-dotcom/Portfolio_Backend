381c8a2b8049d39e813b989ca6b034214b2c5cdb
Ore <aboloresanni001@gmail.com>
2026-04-16 01:32:27 +0100

    heroku

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./portfolio.db")
+DATABASE_URL = os.getenv("JAWSDB_URL", "DATABASE_URL") #"sqlite:///./portfolio.db"
 
 connect_args = {}
 if DATABASE_URL.startswith("sqlite"):
